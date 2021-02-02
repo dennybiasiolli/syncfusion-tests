acccb479ae0c0d4165e25f221538a9a3470bb518
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-02-02 16:23:41 +0100

    using syncfusion components as dedicated plugin

---
+++ src/plugins/syncfusion.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+
+import { ButtonPlugin } from '@syncfusion/ej2-vue-buttons';
+import { GridPlugin } from '@syncfusion/ej2-vue-grids';
+
+import '@syncfusion/ej2-base/styles/material.css';
+import '@syncfusion/ej2-buttons/styles/material.css';
+import '@syncfusion/ej2-calendars/styles/material.css';
+import '@syncfusion/ej2-dropdowns/styles/material.css';
+import '@syncfusion/ej2-inputs/styles/material.css';
+import '@syncfusion/ej2-navigations/styles/material.css';
+import '@syncfusion/ej2-popups/styles/material.css';
+import '@syncfusion/ej2-splitbuttons/styles/material.css';
+import '@syncfusion/ej2-grids/styles/material.css';
+import '@syncfusion/ej2-vue-grids/styles/material.css';
+
+
+Vue.use(ButtonPlugin);
+Vue.use(GridPlugin);

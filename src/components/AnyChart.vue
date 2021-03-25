a4c71eb4eea255fbd9a606bcfd30ac1ad1bb359e
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-03-25 10:38:52 +0100

    anychart: improving stockchart

--- src/components/AnyChart.vue
+++ src/components/AnyChart.vue
@@ -4,21 +4,33 @@
 
 <script>
 import anychart from 'anychart';
-import { mapState } from 'vuex';
 
 export default {
   name: 'any-chart',
   data() {
-    return {};
+    return {
+      chart: null,
+      dataTable: null,
+      ohlcMapping: { open: 'open', high: 'high', low: 'low', close: 'close' },
+      volumeMapping: { x: 'date', value: 'volume' },
+      stockPlot: null,
+      stockSeries: null,
+      volumePlot: null,
+      volumeSeries: null,
+      rangePicker: null,
+      rangeSelector: null,
+    };
   },
   props: {
-    type: {
-      type: String,
+    data: {
+      type: Array,
       required: true,
     },
-    data: {
-      type: [Array, Object],
-      required: true,
+    eventsA: {
+      type: Array,
+    },
+    eventsB: {
+      type: Array,
     },
     title: {
       type: String,
@@ -26,127 +38,88 @@ export default {
     width: String,
     height: String,
   },
-  computed: {
-    ...mapState({
-      chartData: (state) =>
-        state.chartData && {
-          ...state.chartData,
-          values: state.chartData.time_series.map((e) => ({
-            date: new Date(e.date),
-            open: e.open,
-            high: e.high,
-            low: e.low,
-            close: e.close,
-            volume: e.volume,
-          })),
-        },
-    }),
-  },
   watch: {
-    chartData() {
-      this.renderChart();
+    data() {
+      this.setData();
+    },
+    eventsA() {
+      this.setEvents();
+    },
+    eventsB() {
+      this.setEvents();
+    },
+    title() {
+      this.setTitle();
     },
   },
   mounted() {
-    // this.renderChart();
+    this.renderChart();
+    this.chart.listen('eventMarkerClick', function (e) {
+      console.log(e.eventMarker);
+    });
+    this.setTitle();
+    this.setData();
+  },
+  beforeDestroy() {
+    this.chart.removeAllListeners();
   },
   updated() {
-    // this.renderChart();
+    this.setData();
   },
   methods: {
     renderChart() {
-      var dataTable = anychart.data.table('date');
-      dataTable.addData(this.chartData.values);
+      this.dataTable && this.dataTable.remove();
 
-      var ohlcMapping = dataTable.mapAs({
-        x: 'date',
-        open: 'open',
-        high: 'high',
-        low: 'low',
-        close: 'close',
-      });
-      var volumeMapping = dataTable.mapAs({ x: 'date', value: 'volume' });
+      this.dataTable = anychart.data.table('date'); // `date` is the x axis
+      // this.dataTable.addData([...this.data]);
+
+      const ohlcMapping = this.dataTable.mapAs(this.ohlcMapping);
+      const volumeMapping = this.dataTable.mapAs(this.volumeMapping);
 
       // create stock chart
-      var chart = anychart.stock();
+      this.chart = anychart.stock();
 
       // create and setup ohlc series on the first plot
-      var stockPlot = chart.plot(0);
-      var ohlcSeries = stockPlot.ohlc(ohlcMapping);
-      ohlcSeries.name(this.chartData.symbol);
-      ohlcSeries.legendItem().iconType('risingfalling');
+      this.stockPlot = this.chart.plot(0);
+      this.stockSeries = this.stockPlot.ohlc(ohlcMapping);
+      this.stockSeries.legendItem().iconType('risingfalling');
 
       // create and setup volume plot
-      var volumePlot = chart.plot(1);
-      volumePlot.height('30%');
-      volumePlot
-        .yAxis()
-        .labels();
-      var volumeSeries = volumePlot.column(volumeMapping);
-      volumeSeries.name('Volume');
-
-      stockPlot.eventMarkers({
-        type: 'pin',
-        groups: [
-          {
-            format: 'A',
-            data: [
-              {
-                date: '2020-09-14',
-                title: 'Alan',
-                description:
-                  'Cisco announced the acquisition of Audium Corporation.',
-              },
-              {
-                date: '2020-10-14',
-                title: 'Alan',
-                description:
-                  'Cisco announced its intent to acquire PostPath, Inc.',
-              },
-            ],
-          },
-          {
-            format: 'B',
-            data: [
-              {
-                date: '2020-08-14',
-                title: 'Brad',
-                description:
-                  'Cisco announced the acquisition of Audium Corporation.',
-              },
-              {
-                date: '2020-10-17',
-                title: 'Brad',
-                description:
-                  'Cisco announced its intent to acquire PostPath, Inc.',
-              },
-            ],
-          },
-        ],
-      });
+      this.volumePlot = this.chart.plot(1);
+      this.volumePlot.height('30%');
+      this.volumePlot.yAxis().labels();
+      this.volumeSeries = this.volumePlot.column(volumeMapping);
+      this.volumeSeries.name('Volume');
 
       // create scroller series
-      chart.scroller().area(volumeMapping);
+      this.chart.scroller().area(volumeMapping);
 
-      // set container id for the chart
-      chart.container(this.$refs.chartContainer);
+      this.chart.container(this.$refs.chartContainer);
+      this.chart.draw();
 
-      // initiate chart drawing
-      chart.draw();
+      this.rangePicker = anychart.ui.rangePicker();
+      this.rangePicker.render(this.chart);
 
-      // create range picker
-      var rangePicker = anychart.ui.rangePicker();
-      // init range picker
-      rangePicker.render(chart);
-
-      // create range selector
-      var rangeSelector = anychart.ui.rangeSelector();
-      // init range selector
-      rangeSelector.render(chart);
-
-      chart.listen('eventMarkerClick', function (e) {
-        console.log(e.eventMarker);
+      this.rangeSelector = anychart.ui.rangeSelector();
+      this.rangeSelector.render(this.chart);
+    },
+    setData() {
+      this.dataTable && this.dataTable.remove();
+      this.dataTable.addData(this.data);
+      this.setEvents();
+    },
+    setEvents() {
+      this.data.length && this.stockPlot.eventMarkers({
+        type: 'pin',
+        groups: [
+          { format: 'A', data: this.eventsA },
+          { format: 'B', data: this.eventsB },
+        ],
       });
+    },
+    setTitle() {
+      this.chart.title(this.title);
+      this.stockSeries.name(this.title);
     },
   },
 };

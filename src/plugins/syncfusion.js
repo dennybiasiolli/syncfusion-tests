774bb1fe626d2c4fc0aa5917a74c444a735168c8
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-05-18 12:09:46 +0200

    syncfusion: fixing production builds with requiring css styles

--- src/plugins/syncfusion.js
+++ src/plugins/syncfusion.js
@@ -4,16 +4,16 @@ import { ButtonPlugin } from '@syncfusion/ej2-vue-buttons';
 import { GridPlugin } from '@syncfusion/ej2-vue-grids';
 import { ChartPlugin, RangeNavigatorPlugin, StockChartPlugin } from "@syncfusion/ej2-vue-charts";
 
-import '@syncfusion/ej2-base/styles/material.css';
-import '@syncfusion/ej2-buttons/styles/material.css';
-import '@syncfusion/ej2-calendars/styles/material.css';
-import '@syncfusion/ej2-dropdowns/styles/material.css';
-import '@syncfusion/ej2-inputs/styles/material.css';
-import '@syncfusion/ej2-navigations/styles/material.css';
-import '@syncfusion/ej2-popups/styles/material.css';
-import '@syncfusion/ej2-splitbuttons/styles/material.css';
-import '@syncfusion/ej2-grids/styles/material.css';
-import '@syncfusion/ej2-vue-grids/styles/material.css';
+require('@syncfusion/ej2-base/styles/material.css');
+require('@syncfusion/ej2-buttons/styles/material.css');
+require('@syncfusion/ej2-calendars/styles/material.css');
+require('@syncfusion/ej2-dropdowns/styles/material.css');
+require('@syncfusion/ej2-inputs/styles/material.css');
+require('@syncfusion/ej2-navigations/styles/material.css');
+require('@syncfusion/ej2-popups/styles/material.css');
+require('@syncfusion/ej2-splitbuttons/styles/material.css');
+require('@syncfusion/ej2-grids/styles/material.css');
+require('@syncfusion/ej2-vue-grids/styles/material.css');
 
 
 Vue.use(ButtonPlugin);

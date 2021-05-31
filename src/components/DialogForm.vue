1c6211cc3972a1a58d8b167f9413bb2dc45ec2c2
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2021-05-31 14:51:09 +0200

    Grid2: handling Dialog edit with an external component

---
+++ src/components/DialogForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ data }}
+    <p>
+      <input type="text" v-model="data.ListABC" />
+    </p>
+  </div>
+</template>
+
+<script>
+const DialogForm = {
+  name: 'DialogForm',
+  data() {
+    return {
+      data: {},
+    };
+  },
+};
+
+export default DialogForm;
+export const DialogFormTemplate = () => ({ template: DialogForm });
+</script>

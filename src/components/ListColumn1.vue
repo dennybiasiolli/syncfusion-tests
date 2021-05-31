587612f3ce6638abf288c7c0eed94ebbba3c2997
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-05-31 15:52:42 +0200

    Grid: fixing and improving multiselect filter

--- src/components/ListColumn1.vue
+++ src/components/ListColumn1.vue
@@ -3,6 +3,9 @@
 </template>
 
 <script>
+import { createElement } from '@syncfusion/ej2-base';
+import { MultiSelect } from '@syncfusion/ej2-dropdowns';
+
 const ListColumn1 = {
   name: 'ListColumn1',
   data() {
@@ -12,17 +15,53 @@ const ListColumn1 = {
   },
 };
 
+let dropInstance = null;
+let lastDropInstanceValue = [];
+
 export default ListColumn1;
 export const ListColumn1GridColumn = {
+  type: 'string',
   template: () => ({ template: ListColumn1 }),
-  // editType: 'DropDownList',
-  // dataSource: [],
   edit: {
     read(args) {
       return args.value
         .split(',')
         .map((e) => e.trim())
         .filter((e) => e !== '');
+    },
+  },
+  filter: {
+    ui: {
+      create(args) {
+        console.log('create');
+        let flValInput = createElement('input', {
+          className: 'flm-input',
+        });
+        args.target.appendChild(flValInput);
+        dropInstance = new MultiSelect({
+          dataSource: ['alberta', 'belgium', 'china', 'delaware', 'egypt'],
+          fields: { value: 'ListABC', text: 'ListABC' },
+          placeholder: 'Select a value',
+          popupHeight: '200px',
+          mode: 'CheckBox',
+        });
+        dropInstance.appendTo(flValInput);
+      },
+      write(args) {
+        console.log('write args', args, dropInstance);
+        console.log(args.filteredValue);
+        dropInstance.value = lastDropInstanceValue;
+      },
+      read(args) {
+        console.log('read args', args, dropInstance);
+        args.fltrObj.filterSettings.columns = [];
+        args.fltrObj.filterByColumn(
+          args.column.field,
+          'contains',
+          dropInstance.value
+        );
+        lastDropInstanceValue = dropInstance.value;
+      },
     },
   },
   sortComparer: (reference, comparer) => {

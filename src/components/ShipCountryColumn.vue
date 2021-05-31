ca1b5d7875539129972b9bc2e0631073b22ad95f
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2021-05-31 13:39:57 +0200

    ShipCountryColumn: optimizing code

--- src/components/ShipCountryColumn.vue
+++ src/components/ShipCountryColumn.vue
@@ -18,6 +18,14 @@ export default ShipCountryColumn;
 
 let dropInstance = null;
 let countryElem, countryObj;
+
+const countries = [
+  { countryId: 1, countryName: 'Brazil' },
+  { countryId: 2, countryName: 'Belgium' },
+  { countryId: 3, countryName: 'Germany' },
+  { countryId: 4, countryName: 'France' },
+];
+
 export const ShipCountryGridColumn = {
   template: () => ({ template: ShipCountryColumn }),
   edit: {
@@ -26,22 +34,20 @@ export const ShipCountryGridColumn = {
       return countryElem;
     },
     read() {
-      return countryObj.text + 'foo';
-    },
-    destroy() {
-      countryObj.destroy();
+      console.log(countryObj);
+      return countryObj.text;
     },
     write() {
       countryObj = new DropDownList({
-        dataSource: [
-          { countryName: 'United States', countryId: '1' },
-          { countryName: 'Australia', countryId: '2' },
-        ],
+        dataSource: countries,
         fields: { value: 'countryId', text: 'countryName' },
         placeholder: 'Select a country',
         floatLabelType: 'Never',
       });
       countryObj.appendTo(countryElem);
+    },
+    destroy() {
+      countryObj.destroy();
     },
   },
   filter: {
@@ -53,27 +59,27 @@ export const ShipCountryGridColumn = {
         });
         args.target.appendChild(flValInput);
         dropInstance = new MultiSelect({
-          dataSource: ['France', 'Germany', 'Brazil', 'Belgium'],
-          fields: { text: 'ShipCountry', value: 'ShipCountry' },
+          dataSource: countries, // ['France', 'Germany', 'Brazil', 'Belgium'],
+          fields: { value: 'countryName', text: 'countryName' },
           placeholder: 'Select a value',
           popupHeight: '200px',
           mode: 'CheckBox',
         });
         dropInstance.appendTo(flValInput);
       },
-      destroy() {
-        dropInstance.destroy();
-      },
-      write(args) {
-        dropInstance.value = args.filteredValue;
-      },
       read(args) {
         args.fltrObj.filterSettings.columns = [];
         args.fltrObj.filterByColumn(
           args.column.field,
-          'contains',
+          'equal',
           dropInstance.value
         );
+      },
+      write(args) {
+        dropInstance.value = args.filteredValue;
+      },
+      destroy() {
+        dropInstance.destroy();
       },
     },
   },
